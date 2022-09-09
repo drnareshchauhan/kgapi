188e61a95568ef81f7e777d047ebbe010e86b570
drnareshchauhan <drnareshchauhan@gmail.com>
2022-09-09 13:47:41 +0530

    cors

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://psmkg.netlify.app"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+    origin: true,
+  })
+);
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
